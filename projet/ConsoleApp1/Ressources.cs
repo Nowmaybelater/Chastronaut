8cc3f975c654ad1957b02a9bca7d8eaa01465133
Valentine2411 <vlorsery@ensc.fr>
2022-03-14 10:40:43 +0100

    Création classe Ressources

---
+++ projet/ConsoleApp1/Ressources.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    abstract class Ressources
+    {
+        protected int Quantite { get; set; }
+
+        public Ressources()
+        {
+            this.Quantite = 5;
+        }
+        public Ressources(int Quantite)
+        {
+            this.Quantite = Quantite;
+        }
+
+        public abstract void Utiliser();
+    }
+}

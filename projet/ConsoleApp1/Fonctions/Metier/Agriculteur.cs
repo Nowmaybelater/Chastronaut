8e985b81012b6eaacc0afe25521d84e33028f2ca
Tinuviel11 <valentine.lorsery@gmail.com>
2022-03-28 11:31:39 +0200

    modif agriculteur

--- projet/ConsoleApp1/Fonctions/Metier/Agriculteur.cs
+++ projet/ConsoleApp1/Fonctions/Metier/Agriculteur.cs
@@ -52,5 +52,48 @@ namespace ConsoleApp1
             }
             return action;
         }
+
+        public void AgirAutomatiquement (Chats chat, List<Ressources> listeRessources)
+        {
+            Agriculteur A = chat._Fonction as Agriculteur;
+            RessourceAlimentaire R = listeRessources[0] as RessourceAlimentaire;
+
+            //comportement automatique pour se nourrir
+            if (listeRessources[0].Quantite != 0) //le chat va commencer par manger le fruit de façon automatique car c'est le moins nourissant
+            {
+                if (R!=null)
+                {
+                    chat.Manger(listeRessources[0]);
+                }
+            }
+            else if (listeRessources[2].Quantite != 0) //s'il n'y a pas de fruits, le chat mange un gateau
+            {
+                chat.Manger(listeRessources[2]);
+            }
+            else if (listeRessources[3].Quantite != 0) //s'il n'y a ni fruits ni gateaux, le chat mange un poisson
+            {
+                chat.Manger(listeRessources[2]);
+            }
+
+            //comportement automatique pour se divertir
+            if (listeRessources[6].Quantite != 0) //le chat commencer automatiquement par se divertir avec un livre
+            {
+                chat.SeDivertir(listeRessources[6]);
+            }
+
+            if (listeRessources[5].Quantite != 0)//s'il n'y a pas de livre, le chat se divertit avec un film
+            {
+                chat.SeDivertir(listeRessources[5]);
+            }
+
+            //comportement automatique pour se reposer
+            chat.SeReposer();
+
+            //comportement automatique de récolte (propre au chat agriculteur)
+            A.Recolter(chat, listeRessources[0], listeRessources[7]);
+
+            //comportement automatique de plantation (propre au chat agriculteur)
+            A.Planter(chat, );
+        }
     }
 }
